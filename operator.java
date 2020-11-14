f7bc45cb447951c279d909bd9c93f3e130a3d43f
kingusa21221 <74409029+kingusa21221@users.noreply.github.com>
2020-11-14 13:08:10 +0200

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.mavenproject1;
+
+
+public class operator {
+    public int multiplcation(int a,int b)
+    {
+        return a*b;
+    }
+    public int divid(int a,int b)
+    {
+        return a/b;
+    }
+}
