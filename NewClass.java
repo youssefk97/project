3d24eb6ceee2b3d05896ec505364b6f8be7d1774
youssefk97 <74456779+youssefk97@users.noreply.github.com>
2020-11-14 15:31:45 +0200

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Khattab
+ */
+public class NewClass {
+    public int multiplcation(int a,int b)
+    {
+        return a+b;
+    }
+    public int divid(int a,int b)
+    {
+        return a-b;
+    }
+    
+}
